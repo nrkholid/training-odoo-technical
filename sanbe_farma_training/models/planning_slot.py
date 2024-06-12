66015c87af5ab3101154423485a44fe65bb6b25b
Kholid <nrkholid1@gmail.com>
2024-06-12 14:38:27 +0700

    [UPD] Planning Slot Model

--- sanbe_farma_training/models/planning_slot.py
+++ sanbe_farma_training/models/planning_slot.py
@@ -34,7 +34,20 @@ class PlanningSlotTraining(models.Model):
     expected_revenue = fields.Monetary(compute='_compute_expected_revenue', 
                                        string='Expected Revenue', store = True,
                                        currency_field='currency_id')
+    line_ids = fields.One2many('planning.slot.line.training', 'slot_training_id', string='Line')
+    actual_progress = fields.Float(compute='_compute_actual_progress', string='Actual Progress')
     
+    @api.depends('line_ids', 'line_ids.current_progress', 'line_ids.state')
+    def _compute_actual_progress(self):
+        for rec in self:
+            if rec.line_ids:
+                line_ids = rec.line_ids.filtered(lambda x : x.state == 'confirm')
+                actual_progress = line_ids.sorted(key = lambda x : (x.date, x.id))[-1].current_progress \
+                    if line_ids else 0
+                rec.actual_progress = actual_progress
+            else:
+                rec.actual_progress = 0
+
     @api.depends('number_of_mandays', 'amount')
     def _compute_expected_revenue(self):
         for rec in self:
@@ -78,3 +91,21 @@ class PlanningSlotTraining(models.Model):
             else:
                 rec.resource_ids = self.env['resource.resource'].search([])
     
+class PlanningSlotTrainingLine(models.Model):
+    _name = 'planning.slot.training.line'
+    _description = 'Planning Slot Training Line'
+    
+    slot_training_id = fields.Many2one('planning.slot.training', string='Slot Training',
+                                    index = True, copy = False, tracking = True)
+    date = fields.Date('Date', default=lambda self: fields.Date.context_today(self))
+    role_id = fields.Many2one('planning.role.training', string='Role',
+                              related='slot_training_id.role_id')
+    resource_id = fields.Many2one('resource.resource', string='Resource',
+                                related='slot_training_id.resource_id')
+    name = fields.Char('Name', related='slot_training_id.name', store = True)
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirm', 'Confirm'),
+    ], string='State', default = 'draft', index = True)
+    previous_progress = fields.Float('Previous Progress')
+    current_progress = fields.Float('Current Progress')
