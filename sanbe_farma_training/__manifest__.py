2ed27117fbad9d113cb6ed6c693ac18cc561b986
Kholid <nrkholid1@gmail.com>
2024-06-12 08:52:07 +0700

    first commit

---
+++ sanbe_farma_training/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name" : "PT. Sanbe Farma - Training",
+    "description" : "Pelatihan Technical Odoo V17 untuk PT. Sanbe Farma",
+    "author" : "PT. Sanbe Farma, PT. Arkana Solusi Digital",
+    "version" : "17.0.1.0.0",
+    "category" : "Others",
+    "license" : "OPL-1",
+    "depends" : [
+        "base", 
+        "mail",
+        "hr",
+    ],
+    "data" : [
+        "security/ir.model.access.csv",
+        "views/planning_role_view.xml",
+    ],
+    "auto_install" : False, 
+    "installable" : True,
+    "application" : True,
+    "external_dependencies" : {"python" : []}
+}
---
+++ sanbe_farma_training/models/planning_role.py
@@ -0,0 +1,34 @@
+from odoo import _, api, fields, models
+from random import randint
+
+class PlanningRoleTraining(models.Model):
+    _name = 'planning.role.training'
+    _description = 'Planning Role Training'
+    _order = 'name asc'
+    
+    def _get_default_color(self):
+        return randint(1, 11)
+    
+    company_id = fields.Many2one('res.company', string='Company', default = 
+                                 lambda self : self.env.company)
+    currency_id = fields.Many2one('res.currency', string='Currency', 
+                                  related='company_id.currency_id', store = True)
+    point_rate = fields.Integer('Point Rate')
+    amount = fields.Monetary('Amount', curreny_field='currency_id')
+    active = fields.Boolean('Active', default=True)
+    name = fields.Char('Nama Role', index=True)
+    color = fields.Integer('Color', default=_get_default_color)
+    resource_ids = fields.Many2many(comodel_name='resource.resource', 
+                                    relation='planning_resource_ids',
+                                    column1='planning_role_id',
+                                    column2='resource_id',
+                                    string='Resource')
+    
+    sequence = fields.Integer('Sequence')
+
+
+    
+        
+    
+    
+
